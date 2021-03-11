f89c554a128be8fc916c73de05418118a46db4c4
Habibi Mustafa <bibitahabibi@gmail.com>
2021-03-11 15:34:47 +0700

    move to public

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ listenAndServe({ port: 3000 }, async (req) => {
     req.respond({
         status: 200,
         headers: new Headers({ 'content-type': 'text/html' }),
-        body: await Deno.open('./index.html')
+        body: await Deno.open('./public/index.html')
     })
 })
 
