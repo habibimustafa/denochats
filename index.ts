e05b4b801d26361f9e4cae1ae50a50249769e94e
Habibi Mustafa <bibitahabibi@gmail.com>
2021-03-11 16:47:56 +0700

    use router

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import {listenAndServe} from "https://deno.land/std/http/server.ts"
+import {listenAndServe, ServerRequest} from "https://deno.land/std/http/server.ts"
+import {Router} from "./router.ts";
 
-listenAndServe({ port: 3000 }, async (req) => {
-    req.respond({
+listenAndServe({ port: 3000 }, async (req: ServerRequest) => {
+    const router = new Router(req);
+    router.get('/', {
         status: 200,
         headers: new Headers({ 'content-type': 'text/html' }),
         body: await Deno.open('./public/index.html')
     })
 })
 
-console.log("Server running on localhost:3000")
+console.log('Server running on localhost:3000')
